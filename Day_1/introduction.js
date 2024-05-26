4f2efcdd9681e39d7b6bb312923c19f2f1b45655
Swapnil Nandalalsing Pardeshi <126460594+swapnil94pardeshi@users.noreply.github.com>
2024-05-26 16:50:57 +0530

    first day complete

---
+++ Day_1/introduction.js
@@ -0,0 +1,11 @@
+//Write a single line comment which says, comments can make code readable
+
+//Write another single comment which says, Welcome to 30DaysOfJavaScript
+
+/*Write a multiline comment which says, 
+comments can make code readable, easy to reuse and informative */
+
+//Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+
+
---
+++ Day_1/variable.js
@@ -0,0 +1,36 @@
+let variable1= 'swapnil'
+let age=100
+let boole= true
+
+//Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+print(typeof variable1)
+print(typeof age)
+print(typeof boole)
+
+//Declare four variables without assigning values
+
+
+let a=10
+let b=20
+let c=30
+let d=40
+
+//Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+first_name="swapnil"
+last_name="pardshi"
+marital_status="single"
+country="india"
+age="30"
+
+//Declare variables to store your first name, last name, marital status, country and age in a single line
+
+let fname='swapnil',lname='pardeshi',mar_status='single',coun='india',ag=30
+
+//Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+let myAge=30
+let yourAge=35
+
+console.log(myAge, yourage)
